982690ad6c286a42830c59b12d753bfecf770c48
Apolline97 <adelaherat@gmail.com>
2026-02-20 11:26:14 +0100

    feat: Configuración de Django Admin y registro de modelos

--- backend/recetas/admin.py
+++ backend/recetas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Perfil, Receta, Ingrediente, RecetaIngrediente, PlanComida
 
-# Register your models here.
+# Registramos nuestros modelos para que aparezcan en el panel /admin
+admin.site.register(Perfil)
+admin.site.register(Receta)
+admin.site.register(Ingrediente)
+admin.site.register(RecetaIngrediente)
+admin.site.register(PlanComida)
